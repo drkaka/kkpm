e944f964c73217983a63af38c771fe6efba8ad28
LeeQ <leeq@me.com>
2016-03-01 19:32:29 +0800

    Update travis file.

--- db/db_test.go
+++ db/db_test.go
@@ -2,6 +2,17 @@ package db
 
 import "testing"
 
+var (
+	// DBName the PostgresSQL database name.
+	DBName string
+	// DBHost the host address.
+	DBHost string
+	// DBUser the db user.
+	DBUser string
+	//DBPassword the db password.
+	DBPassword string
+)
+
 func prepareDB(t *testing.T) {
 	PrepareDB()
 }
